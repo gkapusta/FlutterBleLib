91909e01b5a0f0e3048e37e149699c080a519412
Leo Huang <leo.h@wbstech.com.au>
2024-05-23 10:56:04 +1000

    Remove unused meta dependency

--- lib/src/util/_transaction_id_generator.dart
+++ lib/src/util/_transaction_id_generator.dart
@@ -1,10 +1,5 @@
-import 'package:meta/meta.dart';
-
 abstract class TransactionIdGenerator {
   static int _id = 0;
-
-  @visibleForTesting
-  static int get id => _id;
 
   static String getNextId() {
     _id++;
